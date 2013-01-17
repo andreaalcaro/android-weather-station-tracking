33f4762acdf8829ad57830262d51cc355b69201d
Anthony Gelibert <anthony.gelibert@me.com>
2013-01-17 22:55:53 +0100

    A best use of Enum for DataTypeEnum.

--- src/fr/iutvalence/android/weathertrack/DataTypeEnum.java
+++ src/fr/iutvalence/android/weathertrack/DataTypeEnum.java
@@ -2,68 +2,42 @@ package fr.iutvalence.android.weathertrack;
 
 /**
  * Enumeration for data types reported by weather stations
- * 
- * @author sebastienjean
  *
+ * @author sebastienjean
  */
 public enum DataTypeEnum
 {
-	/**
-	 * Timestamp
-	 */
-	DATE, 
-	/**
-	 * Primary temperature (often external), in Celsius degrees
-	 */
-	TEMP1, 
-	/**
-	 * Secondary temperature (often internal), in Celsius degrees
-	 */
-	TEMP2,  
-	/**
-	 * Pressure, in hectoPascals
-	 */
-	PRESSURE, 
-	/**
-	 * Wind speed, in meters per second
-	 */
-	WIND_SPEED,	
-	/**
-	 * Wind direction (relative to North), in degrees
-	 */
-	WIND_DIR, 
-	/**
-	 * relative humidity, in percent 
-	 */
-	HYGRO, 
-	/**
-	 * Luminosity, in Lux
-	 */
-	LUX;
-	
-	/**
-	 * Returns the string representation of the unit associated to a given data type.
-	 * @return the string representation of the unit associated to <tt>dataType</tt>
-	 */
-	public String unitToString()
-	{
-		// DATE, TEMP1, TEMP2, PRESSURE, WIND_SPEED, WIND_DIR, HYGRO, LUX;
-		if (this == DATE)
-			return "";
-		if (this == TEMP1)
-			return "°C";
-		if (this == TEMP2)
-			return "°C";
-		if (this == PRESSURE)
-			return "hPa";
-		if (this == HYGRO)
-			return "%";
-		if (this == LUX)
-			return "lux";
-		if (this == WIND_DIR)
-			return "m/s";
-		if (this == WIND_SPEED)
-			return "°";
-		return null;
-	}
+    /** Timestamp */
+    DATE(" "), // NON-NLS
+    /** Primary temperature (often external), in Celsius degrees */
+    TEMP1(" °C"), // NON-NLS
+    /** Secondary temperature (often internal), in Celsius degrees */
+    TEMP2(" °C"), // NON-NLS
+    /** Pressure, in hectoPascals */
+    PRESSURE(" hPa"), // NON-NLS
+    /** Wind speed, in meters per second */
+    WIND_SPEED(" m/s"), // NON-NLS
+    /** Wind direction (relative to North), in degrees */
+    WIND_DIR(" °"), // NON-NLS
+    /** relative humidity, in percent */
+    HYGRO(" %"), // NON-NLS
+    /** Luminosity, in Lux */
+    LUX(" lux"); // NON-NLS
+
+    private final String m_type;
+
+    DataTypeEnum(final String type)
+    {
+        m_type = type;
+    }
+
+    /**
+     * Returns the string representation of the unit associated to a given data type.
+     *
+     * @return the string representation of the unit associated to <tt>dataType</tt>
+     */
+    public String unitToString()
+    {
+        return m_type;
+    }
 }
